7cf3c32016a639886b69316acc73d04ac5a27d3c
Rachanon Petchoo <petchoo2@illinois.edu>
2025-04-14 20:14:08 -0500

    Added: SQL script for creating DB

---
+++ news-recommendation-system/sql/db_setup.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE cs510;
+
+SHOW DATABASES;
+
+USE cs510;
+
+SHOW TABLES;
+
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE news_articles (
+    news_id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT NOT NULL,
+    news_embedding JSON NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE user_profiles (
+    user_id INT PRIMARY KEY,
+    user_embedding JSON NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_interactions (
+    interaction_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    news_id INT NOT NULL,
+    time_spent_seconds INT NOT NULL,  -- Weight of interest
+    viewed_at TIMESTAMP NOT NULL,     -- For interest decay
+
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (news_id) REFERENCES news_articles(news_id) ON DELETE CASCADE
+);
