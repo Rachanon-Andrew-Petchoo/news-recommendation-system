f1664a5e470ee5b762cc725b11e14dcc9c88773c
Rachanon Petchoo <petchoo2@illinois.edu>
2025-05-07 14:18:51 -0500

    Fixed: Make type of mock data consistent

--- news-recommendation-system/app/recommend/page.tsx
+++ news-recommendation-system/app/recommend/page.tsx
@@ -3,31 +3,31 @@ import Link from 'next/link';
 
 const mockRecommendedNews = [
   { 
-    news_id: '1', 
+    news_id: 1, 
     title: 'Bitcoin in the bush - the crypto mine in remote Zambia', 
     description: 'Bitcoin miners will go to remote locations to take advantage of cheap electricity.', 
     urlToImage: 'https://ichef.bbci.co.uk/news/1024/branded_news/583f/live/26541af0-0628-11f0-b773-ddd19e96af91.jpg' 
   },
   { 
-    news_id: '2', 
+    news_id: 2, 
     title: 'Ethereum’s energy consumption drops dramatically', 
     description: 'A revolutionary shift to proof-of-stake reduces energy demands significantly.',
     urlToImage: 'https://ichef.bbci.co.uk/news/1024/branded_news/583f/live/26541af0-0628-11f0-b773-ddd19e96af91.jpg' 
   },
   { 
-    news_id: '3', 
+    news_id: 3, 
     title: 'AI and the Future of Work', 
     description: 'How artificial intelligence is reshaping the job market and workplace dynamics.',
     urlToImage: 'https://ichef.bbci.co.uk/news/1024/branded_news/583f/live/26541af0-0628-11f0-b773-ddd19e96af91.jpg' 
   },
   { 
-    news_id: '4', 
+    news_id: 4, 
     title: 'AI and the Future of Work', 
     description: 'How artificial intelligence is reshaping the job market and workplace dynamics.',
     urlToImage: 'https://ichef.bbci.co.uk/news/1024/branded_news/583f/live/26541af0-0628-11f0-b773-ddd19e96af91.jpg' 
   },
   { 
-    news_id: '5', 
+    news_id: 5, 
     title: 'AI and the Future of Work', 
     description: 'How artificial intelligence is reshaping the job market and workplace dynamics.',
     urlToImage: 'https://ichef.bbci.co.uk/news/1024/branded_news/583f/live/26541af0-0628-11f0-b773-ddd19e96af91.jpg' 
